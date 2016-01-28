61696f798d70576d5e010c5b055f30ace5b4b397
martinvandriel <vandriel@erdw.ethz.ch>
2016-01-28 10:34:17 +0100

    adding elastic timestepping and setup

--- src.jl/RunSimulation3d.jl
+++ src.jl/RunSimulation3d.jl
@@ -1,6 +1,6 @@
 module RunSimulation3d
 
-export runsimulation3d
+export runsimulation3d, runsimulation3d_elastic
 
 using Mesh
 using Nodes
@@ -36,9 +36,59 @@ function runsimulation3d(meshfile)
     h_min = minimum(element_radius)
     
     finaltime = 2 * 2/sqrt(3)
-    @time (un,error) = run_timestepping(x_n,y_n,z_n,Minv,M,Ke,bc_array,dofindex,h_min,tet,finaltime)
+    @time (un,error) = run_timestepping(x_n,y_n,z_n,Minv,Ke,bc_array,dofindex,h_min,tet,finaltime)
     
     return (h_min,error)
+    
+end
+
+
+function runsimulation3d_elastic(meshfile)
+
+    (v_x, v_y, v_z, EToV) = loadmesh(meshfile)
+
+    # set polynomial order
+    p_N = 3
+    println("Setting order $(p_N) polynomials")
+    @match p_N begin
+        1 => tet = p1tetrahedra()
+        2 => tet = p2tetrahedra()
+        3 => tet = p3tetrahedra()
+        _ => error("Polynomial order not supported")
+    end    
+    
+    (dofindex, x_n, y_n, z_n) = buildxyz(v_x, v_y, v_z, EToV, tet)
+    
+    bc = zeros(length(x_n), 3)
+
+    # for pure P-wave in x-direction
+    #bc[:, 1] = dirichlet_bc(0.0, 2.0, -1.0, 3.0, -1.0, 3.0, x_n, y_n, z_n)
+    #bc[:, 2] = dirichlet_bc(0.0, 2.0,  0.0, 2.0, -1.0, 3.0, x_n, y_n, z_n)
+    #bc[:, 3] = dirichlet_bc(0.0, 2.0, -1.0, 3.0,  0.0, 2.0, x_n, y_n, z_n)
+
+    # for pure S-wave in x-direction, polarized in y or z-direction
+    bc[:, 1] = dirichlet_bc(0.0, 2.0,  0.0, 2.0,  0.0, 2.0, x_n, y_n, z_n)
+    bc[:, 2] = dirichlet_bc(0.0, 2.0, -1.0, 3.0, -1.0, 3.0, x_n, y_n, z_n)
+    bc[:, 3] = dirichlet_bc(0.0, 2.0, -1.0, 3.0, -1.0, 3.0, x_n, y_n, z_n)
+
+    ndof = length(x_n)
+
+    (M, Minv) = buildM(tet, v_x, v_y, v_z, EToV, dofindex, ndof)
+    # correct for 'measure' vs 'detJ'
+    M *= 6.
+    Minv /= 6.
+
+    c_prime = build_c_prime(tet, v_x, v_y, v_z, EToV, ndof)
+
+    element_radius = elementradius3d(v_x, v_y, v_z, EToV)
+    h_min = minimum(element_radius)
+    
+    finaltime = 4.
+    @time (un,error) = run_timestepping_elastic(x_n, y_n, z_n, Minv, c_prime,
+                                                bc, dofindex, h_min, tet,
+                                                finaltime)
+    
+    return (h_min, error)
     
 end
 
